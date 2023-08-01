996cd35a6ea65451f53be628cd837ee596c8e232
shubham-sanvariya <shubhamsanvariyait.com>
2023-08-01 14:04:13 +0530

    10th problem Sum two numbers

---
+++ Sum_of_two_integers.java
@@ -0,0 +1,10 @@
+public class Sum_of_two_integers {
+    public static void main(String[] args) {
+
+    }
+
+    public int getSum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+}
