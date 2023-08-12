55f7dfbcc5411d762de72a96b032ca65f5850a6a
shubham-sanvariya <shubhamsanvariyait.com>
2023-08-12 12:16:26 +0530

    22nd problem HouseRobber

--- HouseRobber.java
+++ HouseRobber.java
@@ -5,13 +5,14 @@ public class HouseRobber {
     }
 
     public static int rob(int[] nums) {
-        int[] dp = new int[nums.length + 1];
-        dp[1] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
+        if(nums.length < 2) return nums[0];
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
-
-        System.out.println(dp[nums.length]);
-        return dp[nums.length];
+        System.out.println(dp[nums.length - 1]);
+        return dp[nums.length - 1];
     }
 }
