7df6a31ab2e0ac729d839a27225c9890913b7521
shubham-sanvariya <shubhamsanvariyait.com>
2023-08-18 13:49:13 +0530

    27th problem ReverseLinkedList

---
+++ ReverseLinkedList.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class ReverseLinkedList {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+            System.out.println(prev.val);
+        }
+        return prev;
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+}
+
